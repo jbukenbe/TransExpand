ed1a1c5ef3c856d063c2561b1432a79a833dc488
jbukenbe <jbukenbe@gmail.com>
2017-09-13 14:24:28 -0400

    Results for full model and line parameter tinkering

---
+++ DC_OPF_DetEQ/225a/gamsnext.cmd
@@ -0,0 +1,2 @@
+@echo off
+gmsgennt.cmd "Z:\Documents\GitHub\TransExpand\DC_OPF_DetEQ\225a\" "\\win.pass.psu.edu\jvb5999\Documents\GitHub\TransExpand\DC_OPF_DetEQ\" %1 "Z:\Documents\GitHub\TransExpand\DC_OPF_DetEQ\225a\gamscntr.dat" "C:\Program Files\GAMS\win64\24.5\" "CPLEX"
