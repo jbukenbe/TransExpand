41a8e227406ea6c675296413acceb84769343eca
jbukenbe <jbukenbe@gmail.com>
2017-10-11 14:15:29 -0400

    Work in progress recommender system for scenarios

--- DC_OPF_DynProg/mat_fill_svd.m
+++ DC_OPF_DynProg/mat_fill_svd.m
@@ -1,4 +1,4 @@
-function A_fill = mat_fill_svd(A)
+function A_fill = mat_fill_svd(A,T)
 % This function makes a Singular Value Decomposition of an incomplete
 % matrix and returns the estimated full matrix value
 
@@ -7,29 +7,38 @@ function A_fill = mat_fill_svd(A)
 %1          10/10/2017  JesseB  Initial Version
 
 
-zero_map = (A == 0);
-val_map = ~zero_map;
+[U,S,V] = svd(A);
 
-num_A_val = sum(val_map);
-sum_A_col = sum(A);
-
-mean_A = sum_A_col./num_A_val;
-std_A = zeros(size(mean_A));
-
-for s_idx = 1:size(A,2)
-    A_col = A(val_map(:,s_idx));
-    std_A(s_idx) =  std(A_col);
+S = S(1:9,1:9);
+Sless = S;
+Sless(4:end,:)=0;
+SV = Sless*V';
+x = zeros(9,1);
+T_est = zeros(size(T));
+for t_idx = 1:length(T)
+    T_est(t_idx,:) = scratch(SV',x,T(t_idx,:)');
 end
+A_fill = [A;T_est];
 
-A_mean_fill = A - mean_A.*val_map;
-A_stand = A_mean_fill./std_A;
+%num_A_val = sum(val_map);
+%sum_A_col = sum(A);
+
+%mean_A = sum_A_col./num_A_val;
+%std_A = zeros(size(mean_A));
+
+%for s_idx = 1:size(A,2)
+%    A_col = A(val_map(:,s_idx));
+%    std_A(s_idx) =  std(A_col);
+%end
+
+%A_mean_fill = A - mean_A.*val_map;
+%A_stand = A_mean_fill./std_A;
 
 
-[U,S,V] = svd(A_stand);
-S(:,3:end) = 0;
-A_stand_fill = U*S*V';
+%[U,S,V] = svd(A_stand);
+%A_stand_fill = U*S*V';
 
-A_fill = (A_stand_fill.*std_A)+ mean_A(ones(size(A,1),1),:);
+%A_fill = (A_stand_fill.*std_A)+ mean_A(ones(size(A,1),1),:);
 
 
 end
