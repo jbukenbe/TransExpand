0a2fecd8f8653ad02526f0d9acde05bb18e59820
jbukenbe <jbukenbe@gmail.com>
2017-11-04 19:48:42 -0400

    Adapted to search through HPC data to find best plans

--- DC_OPF_DynProg/pls_val_est.m
+++ DC_OPF_DynProg/pls_val_est.m
@@ -8,6 +8,7 @@ function problem = pls_val_est(problem)
 %1          10/06/2017  JesseB  Initial Version
 %2          10/07/2017  JesseB  Added simple search for new plans
 %3          10/08/2017  JesseB  Added line costs
+%4          11/04/2017  JesseB  Reworked searching for finding min cost plan
 
 % To Do:    Include Line Costs
 %           Avoid duplicate plans
@@ -15,74 +16,81 @@ function problem = pls_val_est(problem)
 %% Initialization
 % Extract needed data from problem
 y = problem.cand_op_cost;
-c_line = problem.params.new_line_cost;
+line_cost = problem.params.new_line_cost;
 x_id = problem.plan_id;
 n_line = problem.params.cand.n;
+n_plans = 2^n_line;
 use_int = problem.params.interaction;
 n_comp = problem.params.n_comp;
+
 % Construct X matrix for regression
-x = de2bi(x_id-1, n_line);
+x_row_n = size(x_id,1);
+x_col_n = n_line;
+if use_int
+    x_col_n = x_col_n*(x_col_n+1)/2;
+end 
+x = zeros(x_row_n, x_col_n);
+x(:,1:n_line) = de2bi(x_id-1, n_line);
 
 % Standardize line data into experimental form -1 = no line 1 = line
-x = (x - .5).*2;
+x(:,1:n_line) = (x(:,1:n_line) - .5).*2;
 
 % Include first order interactions in X matrix
 if use_int
-    for l_idx = 1:(n_line-1)
-        x = horzcat(x,x(:,l_idx).*x(:,(l_idx+1):n_line));    
+    x_col_start = n_line + 1;
+    for l_idx = 1:(n_line-1)        
+        x_col_end = x_col_start + ((n_line-1) - l_idx);
+        x(:,x_col_start:x_col_end) = x(:,l_idx).*x(:,(l_idx+1):n_line);
+        x_col_start = x_col_end + 1;
     end
 end
+
 %% Run PLS regression
 [~, ~, ~, ~, beta_op, pctvar] = plsregress(x,y,n_comp);
 
 
 %% Post Processing
-% Adjust beta for line cost
-beta_full = beta_op;
-beta_full(2:n_line+1) = beta_op(2:n_line+1) + c_line./2;
-%beta_line_sort = sort(beta_full(2:n_line+1));
-%beta_full(2:n_line+1) = beta_full(2:n_line+1)- beta_line_sort(15)*.99;
-
-% Extract inform_sample_n new plans from regression and explore_sample_n plans randomly 
-n_pls_plan = problem.params.inform_sample_n;
-x_fit_id = randperm(2^n_line,n_pls_plan)-1;
-%x_fit = de2bi(x_fit_id,n_line);
-%x_fit = (x_fit - .5).*2;
-
-good_lines = beta_full(2:n_line+1)' < 0;
-good_lines = (good_lines-.5).*2;
-new_inform_plans = good_lines(ones(n_pls_plan,1),:);
-[~,line_rank]=sort(beta_full(2:(n_line+1)).^2);
-plan_flip = -de2bi(0:(n_pls_plan-1),n_line);
-plan_flip = plan_flip(:,line_rank);
-plan_flip = (plan_flip+.5).*2;
-x_fit = new_inform_plans.*plan_flip;
-new_inform_plans = x_fit;
-
-% Get Sample fits for new plans
-%{
+% create full matrix of plans for fit estimation
+x_fit = zeros(n_plans, x_col_n);
+x_fit(:,1:n_line) = de2bi(0:(n_plans-1), n_line);
+x_fit(:,1:n_line) = (x_fit(:,1:n_line) - .5).*2;
 if use_int
+    x_col_start = n_line + 1;
     for l_idx = 1:(n_line-1)
-        x_fit = horzcat(x_fit,x_fit(:,l_idx).*x_fit(:,(l_idx+1):n_line));    
+        x_col_end = x_col_start + ((n_line-1) - l_idx);
+        x_fit(:, x_col_start:x_col_end) = x_fit(:,l_idx).*x_fit(:,(l_idx+1):n_line);
+        x_col_start = x_col_end + 1;
     end
 end
-y_op_fit = [ones(size(x_fit,1),1), x_fit]*beta_op;
-y_op_fit = y_op_fit + x_fit(:,1:n_line)*c_line;
-y_full_fit = [ones(size(x_fit,1),1), x_fit]*beta_full;
-[~,fit_rank]=sort(y_op_fit);
 
-new_inform_plans = x_fit(fit_rank(1:problem.params.inform_sample_n),1:n_line);
-%}
-new_inform_plans = bi2de((new_inform_plans+1)/2)+1;
+% Get fits for all plans operating costs
+y_fit = [ones(size(x_fit,1),1), x_fit]*beta_op;
+[~,fit_rank]=sort(y_fit);
+plot(problem.real_op(fit_rank(1:100000)))
 
-new_explore_plans = randperm(2^n_line,problem.params.explore_sample_n)';
+% Isolate best plans
+best_op_set = fit_rank(1:2000);
+
+% Calculate line costs for best plans
+plan_line_cost = x_fit(best_op_set,1:n_line)*line_cost;
+
+% Sort by line cost
+[~, best_line_subset] = sort(plan_line_cost);
+best_op_set = best_op_set(best_line_subset);
+
+% Calculate full cost for best sets
+best_full_set = best_op_set(1:500);
+[best_full_val, best_full_subset] = sort(problem.cand_full_cost(best_full_set));
+scatter(1:size(best_full_val,1),best_full_val)
+
 %% Output
-problem.new_plan_id = setdiff([new_inform_plans; new_explore_plans], problem.plan_id);
+problem.best_plan_id = best_full_set(best_full_subset(1:100));
+problem.best_plan_full_val = best_full_val(1:100); 
 
 %% Plotting in debug
-
-plot(1:n_comp,cumsum(100*pctvar(2,:)),'-bo')
 %{
+plot(1:n_comp,cumsum(100*pctvar(2,:)),'-bo')
+
 x_fit_id = problem.plan_id;
 x_fit = de2bi(x_fit_id-1, n_line);
 x_fit = (x_fit - .5).*2;
@@ -92,9 +100,9 @@ if use_int
     end
 end
 
-y_fit = [ones(size(x_fit,1),1), x_fit]*beta;
+y_fit = [ones(size(x_fit,1),1), x_fit]*beta_op+de2bi(x_fit_id-1, n_line)*c_line;
 figure
-scatter(y_fit, problem.cand_cost);
+scatter(y_fit, problem.cand_op_cost+de2bi(x_fit_id-1, n_line)*c_line);
 xlabel('fitted value'); ylabel('Actual Plan Cost'); title('Example Predictive Power from 300 Sample Plans'); 
 %}
 end
